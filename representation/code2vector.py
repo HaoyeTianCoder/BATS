c939db9e66a7a908b96df66f90ffcfb1bf5baa11
nih4me <derokabore@gmail.com>
2020-12-17 10:59:24 +0100

    code2vec interface

--- representation/code2vector.py
+++ representation/code2vector.py
@@ -1,10 +1,29 @@
+from code2vec.extractor import Extractor
+import io
+from os import path, mkdir
+import tempfile
 
+EXTRACTOR_JAR = "../representation/code2vec/JavaExtractor/JPredict/target/JavaExtractor-0.0.1-SNAPSHOT.jar"
+MAX_CONTEXTS = 200
+MAX_PATH_LENGTH = 8
+MAX_PATH_WIDTH = 2
 
 class Code2vector:
-    def __init__(self):
-        self.vector = []
+    def __init__(self, model):
+        self.model = model
+        self.vector = None
 
     def convert(self, function):
-        print(function)
-        raise
+        f = tempfile.NamedTemporaryFile(mode='w+', dir='/tmp', delete=True)
+        f.write(function)
+        file_path = f.name
+        f.seek(0)
+        extractor = Extractor(MAX_CONTEXTS, EXTRACTOR_JAR, MAX_PATH_LENGTH, MAX_PATH_WIDTH)
+        paths, _ = extractor.extract_paths(file_path)
+        f.close()
+        result = self.model.predict(paths)
+
+        if result:
+            self.vector = result[0].code_vector
+    
         return self.vector
--- representation/word2vector.py
+++ representation/word2vector.py
@@ -2,18 +2,44 @@ from representation.code2vector import Code2vector
 import pickle
 from representation.CC2Vec import lmg_cc2ftr_interface
 
+# Imports and method code2vec
+from code2vector import Code2vector
+from code2vec.vocabularies import VocabType
+from code2vec.config import Config
+from code2vec.model_base import Code2VecModelBase
+
+MODEL_MODEL_LOAD_PATH = '/Users/abdoulkader.kabore/snt/PhD/01/PatchSimilarity/data/code2vec/models/java14_model/saved_model_iter8.release'
+
 class Word2vector:
     def __init__(self, word2vec):
         self.w2v = word2vec
+        if self.w2v == 'code2vec':
+            # Init and Load the model
+            config = Config(set_defaults=True, load_from_args=True, verify=False)
+            config.MODEL_LOAD_PATH = MODEL_MODEL_LOAD_PATH
+            config.EXPORT_CODE_VECTORS = True
+            model = Word2vector.load_model_code2vec_dynamically(config)
+            config.log('Done creating code2vec model')
+            self.c2v = Code2vector(model)
+            # =======================
+
+    @staticmethod
+    def load_model_code2vec_dynamically(config: Config) -> Code2VecModelBase:
+        assert config.DL_FRAMEWORK in {'tensorflow', 'keras'}
+        if config.DL_FRAMEWORK == 'tensorflow':
+            from code2vec.tensorflow_model import Code2VecModel
+        elif config.DL_FRAMEWORK == 'keras':
+            from code2vec.keras_model import Code2VecModel
+        return Code2VecModel(config)
 
     def convert(self, data_text):
         if self.w2v == 'code2vec':
-            # data_text: ['function A {...}','function B {}']
             test_vector = []
             for i in range(len(data_text)):
                 function = data_text[i]
-                vector = Code2vector().convert(function)
+                vector = self.c2v.convert(function)
                 test_vector.append(vector)
+                break
             return test_vector
 
         if self.w2v == 'cc2vec':
