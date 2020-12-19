81c51e1a44c4d8212fbd12d0a054f2704e151745
howietiangh <363730631@qq.com>
2020-12-20 00:45:01 +0100

    update split patch

--- collect/split_patch.py
+++ collect/split_patch.py
@@ -3,14 +3,16 @@ import os
 path = '/Users/haoye.tian/Documents/University/project/defects4j_patch/'
 
 def slice_patch(path):
+    cnt = 0
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith('.patch'):
                 name = file.split('.')[0]
+                number_diff = 0
+                patch = ''
                 try:
                     with open(os.path.join(root, file)) as f:
-                        number_diff = 0
-                        patch = ''
+
                         for line in f:
                             if line.startswith('diff --git') or line.startswith('Index:'):
                                 if number_diff == 0:
@@ -39,4 +41,75 @@ def slice_patch(path):
                 except Exception as e:
                     print(name)
 
-slice_patch(path)
+                if number_diff > 1:
+                    cnt += 1
+                    print('{} patches with multiple fixes found'.format(cnt))
+
+def slice_patch2(path):
+    cnt = 0
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.patch'):
+                project = root.split('/')[-1]
+                name = file.split('.')[0]
+                id = name
+                number_diff = 0
+                number_AT = 0
+                patch = ''
+                content = False
+                try:
+                    with open(os.path.join(root, file)) as f:
+                        for line in f:
+                            if line.startswith('--- '):
+                                minus_line = line
+                            elif line.startswith('+++ '):
+                                plus_line = line
+                            elif line.startswith('diff '):
+                                if number_diff > 0:
+                                    # save previous patch
+                                    new_path = path.replace('defects4j_patch', 'defects4j_patch_sliced') + project + '/' + id
+                                    new_name = id+'_'+str(number_AT)+'.patch'
+                                    if not os.path.exists(new_path):
+                                        os.makedirs(new_path)
+                                    with open(os.path.join(new_path, new_name), 'w+') as f:
+                                        f.write(minus_line + plus_line + patch)
+
+                                    content = False
+                                else:
+                                    number_diff += 1
+                                    continue
+                            elif line.startswith('@@ '):
+                                if content:
+                                    # save previous patch
+                                    new_path = path.replace('defects4j_patch', 'defects4j_patch_sliced') + project + '/' + id
+                                    new_name = id+'_'+str(number_AT)+'.patch'
+                                    if not os.path.exists(new_path):
+                                        os.makedirs(new_path)
+                                    with open(os.path.join(new_path, new_name), 'w+') as f:
+                                        f.write(minus_line + plus_line + patch)
+
+                                    patch = line
+                                    number_AT += 1
+                                    content = True
+                                else:
+                                    # first @@
+                                    patch = line
+                                    number_AT += 1
+                                    content = True
+                            elif content:
+                                patch += line
+                            else:
+                                continue
+                except Exception as e:
+                    print(e)
+
+                # save last patch
+                new_path = path.replace('defects4j_patch', 'defects4j_patch_sliced') + project + '/' + id
+                new_name = id + '_' + str(number_AT) + '.patch'
+                if not os.path.exists(new_path):
+                    os.makedirs(new_path)
+                with open(os.path.join(new_path, new_name), 'w+') as f:
+                    f.write(minus_line + plus_line + patch)
+
+# slice_patch(path)
+slice_patch2(path)
