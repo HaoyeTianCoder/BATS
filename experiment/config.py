ab7c8f4e322589d5fdf43947884878cf8b9da989
howietiangh <363730631@qq.com>
2020-12-24 01:41:17 +0100

    update path patch

--- experiment/config.py
+++ experiment/config.py
@@ -1,7 +1,9 @@
 class Config:
     def __init__(self):
         self.path_test = '../data/test_case_all_five.pkl'
-        self.path_patch = 'abc'
+        self.path_test_vector = '../data/test_case_all_five.npy'
+
+        self.path_patch_root = '/Users/haoye.tian/Documents/University/project/defects4j_patch_sliced/'
 
 
 if __name__ == '__main__':
