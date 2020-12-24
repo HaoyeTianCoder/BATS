1eb20e8143996f9c068499fd3a790554e7fed74a
howietiangh <363730631@qq.com>
2020-12-24 20:19:35 +0100

    fix cc2vec

--- representation/word2vector.py
+++ representation/word2vector.py
@@ -1,3 +1,5 @@
+import sys, os
+os.path.abspath(os.path.join('..', './representation'))
 from representation.code2vector import Code2vector
 import pickle
 from representation.CC2Vec import lmg_cc2ftr_interface
@@ -7,14 +9,19 @@ from representation.code2vector import Code2vector
 from representation.code2vec.vocabularies import VocabType
 from representation.code2vec.config import Config
 from representation.code2vec.model_base import Code2VecModelBase
+import numpy as np
 
 MODEL_MODEL_LOAD_PATH = '/Users/haoye.tian/Documents/University/data/models/java14_model/saved_model_iter8.release'
+MODEL_CC2Vec = '../representation/CC2Vec/'
 
 class Word2vector:
-    def __init__(self, word2vec, path_patch_root=None):
-        self.w2v = word2vec
+    def __init__(self, test_w2v, patch_w2v, path_patch_root=None):
+        # self.w2v = word2vec
+        self.test_w2v = test_w2v
+        self.patch_w2v = patch_w2v
         self.path_patch_root = path_patch_root
-        if self.w2v == 'code2vec':
+
+        if self.test_w2v == 'code2vec':
             # Init and Load the model
             config = Config(set_defaults=True, load_from_args=True, verify=False)
             config.MODEL_LOAD_PATH = MODEL_MODEL_LOAD_PATH
@@ -23,8 +30,9 @@ class Word2vector:
             config.log('Done creating code2vec model')
             self.c2v = Code2vector(model)
             # =======================
-        elif self.w2v == 'cc2vec':
-            self.dictionary = pickle.load(open('../CC2Vec/dict.pkl', 'rb'))
+        if self.patch_w2v == 'cc2vec':
+            self.dictionary = pickle.load(open(MODEL_CC2Vec+'dict.pkl', 'rb'))
+
     @staticmethod
     def load_model_code2vec_dynamically(config: Config) -> Code2VecModelBase:
         assert config.DL_FRAMEWORK in {'tensorflow', 'keras'}
@@ -34,8 +42,41 @@ class Word2vector:
             from representation.code2vec.keras_model import Code2VecModel
         return Code2VecModel(config)
 
+    def convert_both(self, test_name, test_text, patch_ids):
+        try:
+            #1 test case function
+            function = test_text
+            test_vector = self.c2v.convert(function)
+
+            #2 patch ids
+            # find path_patch
+            if len(patch_ids) == 1 and patch_ids[0].endswith('-one'):
+                project = patch_ids[0].split('_')[0]
+                id = patch_ids[0].split('_')[1].replace('-one', '')
+                path_patch = self.path_patch_root + project + '/' + id + '/'
+                patch_ids = os.listdir(path_patch)
+                path_patch_ids = [path_patch + patch_id for patch_id in patch_ids]
+            else:
+                path_patch_ids = []
+                for name in patch_ids:
+                    project = name.split('_')[0]
+                    id = name.split('_')[1]
+                    patch_id = name.split('_')[1] + '_' + name.split('_')[2] + '.patch'
+                    path_patch = self.path_patch_root + project + '/' + id + '/'
+                    path_patch_ids.append(os.path.join(path_patch, patch_id))
+
+            multi_vector = []
+            for path_patch_id in path_patch_ids:
+                learned_vector = lmg_cc2ftr_interface.learned_feature(path_patch_id, load_model=MODEL_CC2Vec + 'cc2ftr.pt', dictionary=self.dictionary)
+                multi_vector.append(list(learned_vector.flatten()))
+            patch_vector = np.array(multi_vector).mean(axis=0)
+        except Exception as e:
+            raise
+
+        return test_vector, patch_vector
+
     def convert(self, test_name, data_text):
-        if self.w2v == 'code2vec':
+        if self.test_w2v == 'code2vec':
             test_vector = []
             for i in range(len(data_text)):
                 function = data_text[i]
@@ -48,7 +89,7 @@ class Word2vector:
                 test_vector.append(vector)
             return test_vector
 
-        if self.w2v == 'cc2vec':
+        if self.patch_w2v == 'cc2vec':
             patch_vector = []
             for i in range(len(data_text)):
                 patch_ids = data_text[i]
@@ -68,7 +109,10 @@ class Word2vector:
                         path_patch = self.path_patch_root + project +'/'+ id + '/'
                         path_patch_ids.append(os.path.join(path_patch, patch_id))
 
+                multi_vector = []
                 for path_patch_id in path_patch_ids:
-                    learned_vector = lmg_cc2ftr_interface.learned_feature(path_patch_id, load_model='../CC2Vec/cc2ftr.pt', dictionary=self.dictionary)
-                patch_vector.append(learned_vector)
+                    learned_vector = lmg_cc2ftr_interface.learned_feature(path_patch_id, load_model=MODEL_CC2Vec+'cc2ftr.pt', dictionary=self.dictionary)
+                    multi_vector.append(list(learned_vector.flatten()))
+                combined_vector = np.array(multi_vector).mean(axis=0)
+                patch_vector.append(combined_vector)
             return patch_vector
