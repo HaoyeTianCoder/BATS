d5e19db8fd2fdcf80443a498346d829f9479850d
yinghuali <1075407261@qq.com>
2020-12-26 20:23:33 +0800

    update config.py

--- experiment/config.py
+++ experiment/config.py
@@ -3,7 +3,7 @@ class Config:
         self.path_test = '../data/test_case_all_five.pkl'
         # self.path_test_vector = '../data/test_case_function.npy'
 
-        self.path_patch_root = '/Users/haoye.tian/Documents/University/project/defects4j_patch_sliced/'
+        self.path_patch_root = '/Users/lyh/Documents/thy/defects4j_patch_sliced/'
         # self.path_patch_vector = '../data/test_case_function_.npy'
 
         self.path_test_function_patch_vector = '../data/test_function_patch_vector.pickle'
