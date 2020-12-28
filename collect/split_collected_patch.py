6e1fabc4190bbccc879eb11e16ef9571b71cdbf3
howietiangh <363730631@qq.com>
2020-12-28 18:34:26 +0100

    reformat patch

--- collect/split_collected_patch.py
+++ collect/split_collected_patch.py
@@ -1,6 +1,6 @@
 import os
+import shutil
 
-path = '/Users/haoye.tian/Documents/University/data/PatchCollectingV1ISSTA/'
 
 def slice_patch(path):
     cnt = 0
@@ -50,8 +50,8 @@ def slice_patch2(path):
     for root, dirs, files in os.walk(path):
         for file in files:
             if file.endswith('.patch'):
-                id = root.split('/')[-1]
-                project = root.split('/')[-2]
+                id = root.split('/')[-2]
+                project = root.split('/')[-3]
                 name = file.split('.')[0]
                 number_diff = 0
                 number_AT = 0
@@ -61,6 +61,18 @@ def slice_patch2(path):
                     with open(os.path.join(root, file)) as f:
                         for line in f:
                             if line.startswith('--- ') or line.startswith('-- '):
+                                if number_diff > 0:
+                                    # save previous patch
+                                    new_path = root.replace('PatchCollectingV1ISSTA', 'PatchCollectingV1ISSTA_sliced')
+                                    new_name = name+'-'+str(number_AT)+'.patch'
+                                    if not os.path.exists(new_path):
+                                        os.makedirs(new_path)
+                                    with open(os.path.join(new_path, new_name), 'w+') as f:
+                                        f.write(minus_line + plus_line + patch)
+
+                                    content = False
+                                else:
+                                    number_diff += 1
                                 minus_line = line
                             elif line.startswith('+++ ') or line.startswith('++ '):
                                 plus_line = line
@@ -97,4 +109,38 @@ def slice_patch2(path):
                 with open(os.path.join(new_path, new_name), 'w+') as f:
                     f.write(minus_line + plus_line + patch)
 
+def combine_patch(path):
+    cnt = 0
+    for root, dirs, files in os.walk(path):
+        if len(files) > 1 and files[0].endswith('.patch'):
+            frag = files[0].split('-')
+            new_name = '-'.join([frag[0].split('_')[0], frag[1], frag[2], frag[3]])
+            new_str_patch = ''
+            for file in files:
+                with open(os.path.join(root, file), 'r+') as f:
+                    new_str_patch += ''.join(f.readlines())
+            for file in files:
+                os.remove(os.path.join(root, file))
+
+            # save new patch
+            with open(os.path.join(root, new_name), 'w+') as f:
+                f.write(new_str_patch)
+
+def move(path):
+    cnt = 0
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            if file.endswith('.patch'):
+                if '_' in file:
+                    patchid = file.split('_')[0]
+                else:
+                    patchid = file.split('-')[0]
+                new_root = root.replace('PatchCollectingV1', 'PatchCollectingV1ISSTA') + '/'+patchid
+                if not os.path.exists(new_root):
+                    os.makedirs(new_root)
+                shutil.copy(os.path.join(root, file), os.path.join(new_root, file))
+
+# move('/Users/haoye.tian/Documents/University/data/PatchCollectingV1/')
+path = '/Users/haoye.tian/Documents/University/data/PatchCollectingV1ISSTA/'
+# combine_patch(path)
 slice_patch2(path)
