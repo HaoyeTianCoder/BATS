896631d5322017062eb930ec4b21094331fbe2ec
howietiangh <363730631@qq.com>
2021-01-06 01:08:49 +0100

    update

--- experiment/config.py
+++ experiment/config.py
@@ -1,13 +1,19 @@
 class Config:
     def __init__(self):
-        self.path_test = '../data/test_case_all_five.pkl'
+        self.path_test = '../data/test_case_all.pkl'
         # self.path_test_vector = '../data/test_case_function.npy'
 
-        self.path_patch_root = '/Users/lyh/Documents/thy/defects4j_patch_sliced/'
-        # self.path_patch_vector = '../data/test_case_function_.npy'
+        # self.path_patch_root = '/Users/lyh/Documents/thy/defects4j_patch_sliced/'
+        self.path_patch_root = '/Users/haoye.tian/Documents/University/project/defects4j_patch_sliced/'
+        self.path_collected_patch = '/Users/haoye.tian/Documents/University/data/PatchCollectingV1ISSTA_sliced/'
 
-        self.path_test_function_patch_vector = '../data/test_function_patch_vector.pickle'
+        self.path_test_function_patch_vector = '../data/vector_case_cc2vec.pickle'
+        self.patch_w2v = 'cc2vec'
 
+        # self.path_test_function_patch_vector = '../data/vector_case_bert.pickle'
+        # self.patch_w2v = 'bert'
 
+        # self.path_test_function_patch_vector = '../data/vector_case_str.pickle'
+        # self.patch_w2v = 'str'
 if __name__ == '__main__':
     Config()
