45a6b2b59c31bbac74acd3a17919fd67b98850f7
howietiangh <363730631@qq.com>
2021-01-29 16:54:07 +0100

    delete useless link

--- experiment/config.py
+++ experiment/config.py
@@ -1,12 +1,12 @@
 class Config:
     def __init__(self):
         self.path_test = '../data/test_case_all.pkl'
-        # self.path_test_vector = '../data/test_case_function.npy'
+        # self.path_test = '../data/test_one_case_all.pkl'
 
-        # self.path_patch_root = '/Users/lyh/Documents/thy/defects4j_patch_sliced/'
         self.path_patch_root = '/Users/haoye.tian/Documents/University/project/defects4j_patch_sliced/'
         self.path_collected_patch = '/Users/haoye.tian/Documents/University/data/PatchCollectingV1ISSTA_sliced/'
 
+        # self.path_test_function_patch_vector = '../data/vector_one_case_cc2vec.pickle'
         self.path_test_function_patch_vector = '../data/vector_case_cc2vec.pickle'
         self.patch_w2v = 'cc2vec'
 
@@ -14,6 +14,7 @@ class Config:
         # self.patch_w2v = 'bert'
 
         # self.path_test_function_patch_vector = '../data/vector_case_str.pickle'
-        # self.patch_w2v = 'str'
+        # self.patch_w2v = 'string'
+
 if __name__ == '__main__':
     Config()
