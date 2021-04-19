01a62742be1435f9944f69b707a210d4bcfaa94c
haoye tian <363730631@qq.com>
2021-04-19 22:50:22 +0200

    after rebuttal

--- experiment/main.py
+++ experiment/main.py
@@ -115,20 +115,22 @@ class Experiment:
         # patch_bert_vector.patch_bert()
 
         # RQ1: validate hypothesis
-        clu = cluster(self.original_dataset, self.test_name, self.patch_name, self.test_vector, self.patch_vector, method='biKmeans', number=40)
-        clu.validate()
+        # clu = cluster(self.original_dataset, self.test_name, self.patch_name, self.test_vector, self.patch_vector, method='biKmeans', number=40)
+        # clu.validate()
 
         eval = evaluation(self.patch_w2v, self.original_dataset, self.test_name, self.test_vector, self.patch_vector, self.exception_type)
 
         # RQ2: evaluate on developer's patches of defects4j
-        eval.evaluate_defects4j_projects()
+        # eval.evaluate_defects4j_projects()
 
         # RQ3-1: evaluate BATS on the generated patches of APR tools. use cc2vec representation(patch_w2v='cc2vec') and cosine distance
-        eval.predict_collected_projects(path_collected_patch=self.path_generated_patch, cut_off=0.8, distance_method=distance.cosine, patchsim=False)
+        # eval.predict_collected_projects(path_collected_patch=self.path_generated_patch, cut_off=0.8, distance_method=distance.cosine, comparison = 'ASE2020', patchsim=False )
 
-        # RQ3-2: evaluate BATS on patchsim dataset. use bert representation(patch_w2v='bert') and euclidean distance
-        eval.predict_collected_projects(path_collected_patch='/Users/haoye.tian/Documents/University/data/PatchSimISSTA_sliced/'
-                                        , cut_off=0.7, distance_method=distance.euclidean, patchsim=True)
+        # RQ3-2: improve ML-based approach
+        # eval.improve_ML(path_collected_patch=self.path_generated_patch, cut_off=0.8, distance_method=distance.cosine, kfold=5, algorithm='rf', method='combine00')
+
+        # RQ3-3: evaluate BATS on patchsim dataset. use bert representation(patch_w2v='bert') and euclidean distance
+        eval.predict_collected_projects(path_collected_patch='/Users/haoye.tian/Documents/University/data/PatchSimISSTA_sliced/', cut_off=0.0, distance_method=distance.euclidean, patchsim=True,)
 
 if __name__ == '__main__':
     config = Config()
