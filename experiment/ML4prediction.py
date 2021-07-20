bc609912073261073e38270ad9863a35c725f7a0
haoye tian <363730631@qq.com>
2021-07-20 22:29:11 +0200

    generate confusion for comparison

--- experiment/ML4prediction.py
+++ experiment/ML4prediction.py
@@ -17,6 +17,16 @@ class MlPrediction:
 
         self.algorithm = algorithm
 
+    def confusion_matrix(self, y_pred, y_test):
+        for i in range(1, 100):
+            y_pred_tn = [1 if p >= i / 100.0 else 0 for p in y_pred]
+            tn, fp, fn, tp = confusion_matrix(y_test, y_pred_tn).ravel()
+            print('i:{}'.format(i / 100), end=' ')
+            # print('TP: %d -- TN: %d -- FP: %d -- FN: %d' % (tp, tn, fp, fn))
+            recall_p = tp / (tp + fn)
+            recall_n = tn / (tn + fp)
+            print('+Recall: {:.3f}, -Recall: {:.3f}'.format(recall_p, recall_n))
+
     def predict(self):
         accs, prcs, rcs, f1s, aucs = list(), list(), list(), list(), list()
         rcs_p, rcs_n = list(), list()
@@ -28,6 +38,15 @@ class MlPrediction:
         scaler = StandardScaler().fit(x_train)
         x_train = scaler.transform(x_train)
         x_test = scaler.transform(x_test)
+
+        # # deduplicate, deprecated
+        # x_test_unique = [list(x_test[i]) for i in range(len(x_test))]
+        # x_train_unique = []
+        # for i in range(len(x_train)):
+        #     if list(x_train[i]) in x_test_unique:
+        #         continue
+        #     else:
+        #         x_train_unique.append(x_train[i])
 
         print('train data: {}, test data: {}'.format(len(x_train), len(x_test)))
 
@@ -46,25 +65,29 @@ class MlPrediction:
 
         print('1. ML:')
         auc_, recall_p, recall_n, acc, prc, rc, f1 = self.evaluation_metrics(y_true=y_test, y_pred_prob=y_pred)
+        # self.confusion_matrix(y_pred, y_test)
 
         # # combine ML-based and BATs
-        # print('2. Combine:')
-        # y_pred_prob_combine = []
-        # BATs, ML = 0, 0
-        # for i in range(len(self.y_pred_bats)):
-        #     if self.test_case_similarity_list[i] >= 0.9:
-        #         y_pred_prob_combine.append(self.y_pred_bats[i])
-        #         BATs += 1
-        #     else:
-        #         y_pred_ML = clf.predict_proba(x_test[i].reshape(1, -1))[:, 1]
-        #         y_pred_prob_combine.append(y_pred_ML)
-        #         ML += 1
-        # print('BATs:{}, ML:{}'.format(BATs, ML))
-        # auc_, recall_p, recall_n, acc, prc, rc, f1 = self.evaluation_metrics(y_true=y_test, y_pred_prob=y_pred_prob_combine)
+        print('2. Combine(replace):')
+        y_pred_prob_combine = []
+        BATs, ML = 0, 0
+        for i in range(len(self.y_pred_bats)):
+            if self.test_case_similarity_list[i] >= 0.9:
+                y_pred_prob_combine.append(self.y_pred_bats[i])
+                BATs += 1
+            else:
+                y_pred_ML = clf.predict_proba(x_test[i].reshape(1, -1))[:, 1]
+                y_pred_prob_combine.append(y_pred_ML)
+                ML += 1
+        print('BATs:{}, ML:{}'.format(BATs, ML))
+        auc_, recall_p, recall_n, acc, prc, rc, f1 = self.evaluation_metrics(y_true=y_test, y_pred_prob=y_pred_prob_combine)
+        # self.confusion_matrix(y_pred_prob_combine, y_test)
 
-        # average ML-based and BATs
-        y_pred = (y_pred + self.y_pred_bats)/2.0
-        auc_, recall_p, recall_n, acc, prc, rc, f1 = self.evaluation_metrics(y_true=y_test, y_pred_prob=y_pred)
+        # average ML-based and BATs, deprecated
+        # print('3. Combine(average):')
+        # y_pred = (y_pred + self.y_pred_bats)/2.0
+        # auc_, recall_p, recall_n, acc, prc, rc, f1 = self.evaluation_metrics(y_true=y_test, y_pred_prob=y_pred)
+        # self.confusion_matrix(y_pred, y_test)
 
     def evaluation_metrics(self, y_true, y_pred_prob):
         fpr, tpr, thresholds = roc_curve(y_true=y_true, y_score=y_pred_prob, pos_label=1)
