f54a68d0f98ca2f3f1dcb01d008cd688ee61e60f
haoye tian <363730631@qq.com>
2022-01-08 21:10:14 +0100

    none

--- experiment/ML4prediction.py
+++ experiment/ML4prediction.py
@@ -7,7 +7,7 @@ from sklearn.metrics import roc_curve, auc, accuracy_score, recall_score, precis
 from sklearn.naive_bayes import GaussianNB
 
 class MlPrediction:
-    def __init__(self, x_train, y_train, x_test, y_test, y_pred_bats, test_case_similarity_list, algorithm='lr', ):
+    def __init__(self, x_train, y_train, x_test, y_test, y_pred_bats, test_case_similarity_list, algorithm='lr', comparison=True, cutoff=0.8):
         self.x_train = x_train
         self.y_train = y_train
         self.x_test = x_test
@@ -16,6 +16,8 @@ class MlPrediction:
         self.test_case_similarity_list = test_case_similarity_list
 
         self.algorithm = algorithm
+        self.comparison = comparison
+        self.cutoff = cutoff
 
     def confusion_matrix(self, y_pred, y_test):
         for i in range(1, 100):
@@ -27,7 +29,7 @@ class MlPrediction:
             recall_n = tn / (tn + fp)
             print('+Recall: {:.3f}, -Recall: {:.3f}'.format(recall_p, recall_n))
 
-    def predict(self):
+    def predict(self, comparison=False):
         accs, prcs, rcs, f1s, aucs = list(), list(), list(), list(), list()
         rcs_p, rcs_n = list(), list()
 
@@ -52,7 +54,7 @@ class MlPrediction:
 
         clf = None
         if self.algorithm == 'lr':
-            clf = LogisticRegression(solver='lbfgs',).fit(X=x_train, y=y_train)
+            clf = LogisticRegression().fit(X=x_train, y=y_train)
         elif self.algorithm == 'dt':
             clf = DecisionTreeClassifier().fit(X=x_train, y=y_train, sample_weight=None)
         elif self.algorithm == 'rf':
@@ -68,20 +70,21 @@ class MlPrediction:
         # self.confusion_matrix(y_pred, y_test)
 
         # # combine ML-based and BATs
-        print('2. Combine(replace):')
-        y_pred_prob_combine = []
-        BATs, ML = 0, 0
-        for i in range(len(self.y_pred_bats)):
-            if self.test_case_similarity_list[i] >= 0.9:
-                y_pred_prob_combine.append(self.y_pred_bats[i])
-                BATs += 1
-            else:
-                y_pred_ML = clf.predict_proba(x_test[i].reshape(1, -1))[:, 1]
-                y_pred_prob_combine.append(y_pred_ML)
-                ML += 1
-        print('BATs:{}, ML:{}'.format(BATs, ML))
-        auc_, recall_p, recall_n, acc, prc, rc, f1 = self.evaluation_metrics(y_true=y_test, y_pred_prob=y_pred_prob_combine)
-        # self.confusion_matrix(y_pred_prob_combine, y_test)
+        if self.cutoff == 0.6:
+            print('2. Combine(replace):')
+            y_pred_prob_combine = []
+            BATs, ML = 0, 0
+            for i in range(len(self.y_pred_bats)):
+                if self.test_case_similarity_list[i] >= 0.9:
+                    y_pred_prob_combine.append(self.y_pred_bats[i])
+                    BATs += 1
+                else:
+                    y_pred_ML = clf.predict_proba(x_test[i].reshape(1, -1))[:, 1]
+                    y_pred_prob_combine.append(y_pred_ML)
+                    ML += 1
+            print('BATs:{}, ML:{}'.format(BATs, ML))
+            auc_, recall_p, recall_n, acc, prc, rc, f1 = self.evaluation_metrics(y_true=y_test, y_pred_prob=y_pred_prob_combine)
+            # self.confusion_matrix(y_pred_prob_combine, y_test)
 
         # average ML-based and BATs, deprecated
         # print('3. Combine(average):')
@@ -90,10 +93,17 @@ class MlPrediction:
         # self.confusion_matrix(y_pred, y_test)
 
     def evaluation_metrics(self, y_true, y_pred_prob):
+        threshold = 0.5
         fpr, tpr, thresholds = roc_curve(y_true=y_true, y_score=y_pred_prob, pos_label=1)
         auc_ = auc(fpr, tpr)
 
-        y_pred = [1 if p >= 0.5 else 0 for p in y_pred_prob]
+        if self.comparison and self.cutoff == 0.8:
+            if self.algorithm == 'lr':
+                threshold = 0.07
+            elif self.algorithm == 'rf':
+                threshold = 0.37
+
+        y_pred = [1 if p >= threshold else 0 for p in y_pred_prob]
         acc = accuracy_score(y_true=y_true, y_pred=y_pred)
         prc = precision_score(y_true=y_true, y_pred=y_pred)
         rc = recall_score(y_true=y_true, y_pred=y_pred)
