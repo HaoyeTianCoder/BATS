673843b28a4d1b9504cd973e1d76226fe24fe058
haoyetiancoder <363730631@qq.com>
2022-08-24 21:36:40 +0200

    update

--- experiment/config.py
+++ experiment/config.py
@@ -2,9 +2,10 @@ class Config:
     def __init__(self):
         # the orginal data of test case name, test function, associated patch including 'single test case' and 'full' versions.
         self.path_test = '../data/test_case_all.pkl'
-        # developers' patches in defects4j and generated patches of APR tools
+        # developers' patches in defects4j
         self.path_patch_root = '../data/defects4j_patch_sliced/'
 
+        #  generated patches of APR tools
         self.path_generated_patch = '/Users/haoye.tian/Documents/University/data/BATS_DataSet/PatchCollectingV1_sliced/'
         # for Naturalness
         # self.path_generated_patch = '/Users/haoye.tian/Documents/ISSTA2022withTextUnique/'
